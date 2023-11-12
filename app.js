c5f55d940fab137a8c3b90eea40dd477ba61e5d2
Sevo <betont.online@googlemail.com>
2023-11-13 00:52:21 +0100

    timeout

--- app.js
+++ app.js
@@ -21,3 +21,28 @@ const counter2 = document.getElementById("sevo-counter-2");
 counter2.addEventListener("counterChanged", (evt) => {
   console.log("counter2", "count:", evt.detail.count);
 });
+
+class Timeout extends EventTarget {
+  constructor(time = 1000) {
+    super();
+    this._time = time;
+  }
+
+  run() {
+    const startTime = new Date().getTime();
+    setTimeout(() => {
+      const endTime = new Date().getTime();
+      this.dispatchEvent(
+        new CustomEvent("timeout", {
+          detail: { timeDiff: endTime - startTime },
+        })
+      );
+    }, this._time);
+  }
+}
+
+const t = new Timeout();
+t.addEventListener("timeout", (evt) => {
+  console.log("timeout", "time:", evt.detail.timeDiff);
+});
+t.run();
